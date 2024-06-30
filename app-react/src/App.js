7cbb63bc2d509e8b565d5545680cc556bc19eaa3
Felipe34515 <f.rueda4@uniandes.edu.co>
2024-06-30 17:51:34 -0500

    Primeros avances

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,10 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+const TURNS = {	
+  X: "x",
+  O: "o"
+}
+
+
+export const Square = ({ children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard()
+  }
+  return (
+    <div className={className} onClick={handleClick}>
+        {children}
+    </div>
+  )
+}
+
 
 function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const [turn, setTurn] = useState(TURNS.X);
+  const updateBoard = () => {
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn (newTurn)
+  }
+  
+
   return (
-    <div className="App">
-      <h1>Juego De Triqui</h1>
+    <div>
+      <header className="App-header">
+        <h1>Juego de Triqui</h1>
+      </header>
+      <h2 className="App-header">Por Felipe Rueda</h2>
+        <main className='board'>
+          <section className='game'>
+            {
+              board.map((_, index) => {
+                return (
+                  <Square key={index} index={index} updateBoard={updateBoard}>
+                    {board[index]}
+                    </Square>
+
+                  )
+                })
+            }
+          </section>
+          <section className='turn'>
+            <Square isSelected={ turn === TURNS.X} >{TURNS.X}</Square>
+            <Square isSelected={ turn === TURNS.O} >{TURNS.O}</Square>
+          </section>
+        </main>
     </div>
   );
 }
